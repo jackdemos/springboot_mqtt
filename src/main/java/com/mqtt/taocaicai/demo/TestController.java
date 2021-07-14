a3014e3642fb3d6e742b3a236bd046d9a74365a0
oakley <cqfangchaoit@sina.com>
2021-07-15 03:05:26 +0800

    first commit

---
+++ src/main/java/com/mqtt/taocaicai/demo/TestController.java
@@ -0,0 +1,18 @@
+package com.mqtt.taocaicai.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+  @Autowired
+  private MqttGateway mqttGateway;
+
+  @RequestMapping("/sendMqtt")
+  public String sendMqtt(String sendData,String topic) {
+    mqttGateway.sendToMqtt(sendData, topic);
+    return "OK";
+  }
+}
---
+++ src/test/java/com/mqtt/taocaicai/demo/MqqClientApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mqtt.taocaicai.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqqClientApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
